4b46cfca0b877d96cdecb341d1fd33f00b8c6113
Morné le Roux <mornelerx@gmail.com>
2024-12-17 16:11:21 +0200

    Update custom_background.dart

--- lib/core/widgets/custom_background.dart
+++ lib/core/widgets/custom_background.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 import '../usecases/is_dark_mode.dart';
 
@@ -22,15 +21,15 @@ class CustomBackground extends StatelessWidget {
         //   ],
         // ),
       ),
-      child: Opacity(
-        opacity: isDarkMode(context) ? 0.3 : 0.5,
-        child: SvgPicture.asset(
-          isDarkMode(context)
-              ? "assets/background_dark.svg"
-              : "assets/background_light.svg",
-          fit: BoxFit.cover,
-        ),
-      ),
+      // child: Opacity(
+      //   opacity: isDarkMode(context) ? 0.3 : 0.5,
+      //   child: SvgPicture.asset(
+      //     isDarkMode(context)
+      //         ? "assets/background_dark.svg"
+      //         : "assets/background_light.svg",
+      //     fit: BoxFit.cover,
+      //   ),
+      // ),
     );
   }
 }
