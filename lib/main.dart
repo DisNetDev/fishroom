1622259438d321108a6e358ae79bfdb5777a33da
Morné le Roux <mornelerx@gmail.com>
2025-02-19 16:59:46 +0200

    added sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-// ignore_for_file: prefer-match-file-name
-
 import 'package:fishroom/core/theme/slider_theme.dart';
 import 'package:fishroom/features/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:toastification/toastification.dart';
 import 'core/repositories/supabase_repository.dart';
@@ -29,27 +29,46 @@ void main() async {
         localStorage: MySecureStorage(),
       ));
 
-  runApp(MultiRepositoryProvider(
-    providers: [
-      //Global Repositories
-      RepositoryProvider(create: (context) => SupabaseRepository()),
-    ],
-    child: MultiBlocProvider(
-      providers: [
-        //Global Blocs
-        BlocProvider(
-            create: (context) => TanksCubit(
-                supabaseRepository: context.read<SupabaseRepository>())),
-        BlocProvider(
-            create: (context) => AppCubit(context.read<SupabaseRepository>())),
-        BlocProvider(
-            create: (context) =>
-                InhabitantsCubit(context.read<SupabaseRepository>())),
-      ],
-      child: ToastificationWrapper(
-          child: KeyboardVisibilityProvider(child: MainApp())),
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://09181f12e7794ff303bdc7f88309ca26@o4508846445297664.ingest.us.sentry.io/4508846446673920';
+      options.attachScreenshot = true;
+      options.environment = appFlavor;
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(
+      SentryWidget(
+        child: MultiRepositoryProvider(
+          providers: [
+            //Global Repositories
+            RepositoryProvider(create: (context) => SupabaseRepository()),
+          ],
+          child: MultiBlocProvider(
+            providers: [
+              //Global Blocs
+              BlocProvider(
+                  create: (context) => TanksCubit(
+                      supabaseRepository: context.read<SupabaseRepository>())),
+              BlocProvider(
+                  create: (context) =>
+                      AppCubit(context.read<SupabaseRepository>())),
+              BlocProvider(
+                  create: (context) =>
+                      InhabitantsCubit(context.read<SupabaseRepository>())),
+            ],
+            child: ToastificationWrapper(
+                child: KeyboardVisibilityProvider(child: MainApp())),
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
 
 final SupabaseClient supabase = Supabase.instance.client;
